219f2c807ed589f46f53df85f69b0003e8dde139
somesh_codes <someshup007@gmail.com>
2026-05-31 12:30:28 +0530

    loops for array

--- array/arrayi.cpp
+++ array/arrayi.cpp
@@ -2,7 +2,20 @@
 using namespace std;
 
 int main(){
-    int marks[5]= {99,100,55,66,77};
-    int price[]={98,99,105,67,30.00}; 
+    
+    //2-int marks[5]= {99,100,55,66,77};
+    //int size = 5;
+    //loops : 0 to size-1
+    //for (int i=0; i<size; i++){
+      //  cout << marks[i] << endl;
+
+    } 
+   // 1-int price[]={98,99,105,67,30}; 
+    //marks[0] = 101;
+    //cout << marks[0] <<endl;
+    //cout << marks[1];
+    //cout << marks[2];
+    //out << marks[3];
+    //cout << marks[4];
     return 0;
 }
