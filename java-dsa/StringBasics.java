4f8724f19b617fa5281e4f5b897badbd2b13ac4f
somesh_codes <someshup007@gmail.com>
2026-07-27 15:32:29 +0530

    java_strings

---
+++ java-dsa/StringBasics.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class StringBasics{
+    public static void main(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("provide the string content:");
+
+    }
+}
