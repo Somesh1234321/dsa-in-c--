e44a62be6c75c42a9b49c87af2dc77504f4f301a
somesh_codes <someshup007@gmail.com>
2026-08-16 23:50:44 +0530

    edited

--- java-dsa/StringBasics.java
+++ java-dsa/StringBasics.java
@@ -2,7 +2,10 @@ import java.util.Scanner;
 public class StringBasics{
     public static void main(){
         Scanner sc = new Scanner(System.in);
-        System.out.println("provide the string content:");
+        System.out.println("Provide the string content ");
+        String str = sc.nextline();
+        System.out.println("value of nextline:" + str);
+
 
     }
 }
