b93cfd4c0e9d078d1cafbc5231dd67a143d06b4a
somesh_codes <someshup007@gmail.com>
2026-09-13 23:29:14 +0530

    kedaneedit

--- array/vector.cpp
+++ array/vector.cpp
@@ -1,11 +1,38 @@
 #include <iostream>
 #include <vector>
 using  namespace std;
-
 int main (){
-    vector<int>vec;
+/**
+int main (){
+    vector<int>vec (5,0);
+    cout << vec[0] << endl;
+    cout << vec[1] << endl;
+    cout << vec[2] << endl;
+    cout << vec[3] << endl;
+    cout << vec[4] << endl;
     return 0;
-    cout << vec[0];
-    cout << vec[1];
 
+int main(){
+    vector<char> vec={'a','b','c','d','e'};
+    cout <<"size: "<< vec.size() << endl;
+    for(char val: vec){
+        cout << val << endl;
+    }
+        return 0;**/
+        vector<int> vec;
+        vec.push_back(0);
+        vec.push_back(1);
+        vec.push_back(2);
+        vec.push_back(3);
+        cout << "size: " << vec.size() << endl;
+        cout << "capacity: " << vec.capacity() << endl;
+        return 0;
+
+
+    
+   
+      
+    
 }
+   
+    
