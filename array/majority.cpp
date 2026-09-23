57729917a8031ab0dcbc8b5d7d61d0788acf6949
somesh_codes <someshup007@gmail.com>
2026-09-23 23:56:44 +0530

    ERROR_FIND

--- array/majority.cpp
+++ array/majority.cpp
@@ -23,3 +23,19 @@ public:
         return -1;
     }
 };
+include <iostream>
+using namespace std;
+int main(){
+    int nums[]= {5,10,15,20,25,30};
+    int size = 6;
+    int smallest = INT8_MAX;
+    for(int i=0;i<size;i++){
+        if(nums[i] < smallest){
+            smallest = nums[i];
+        }
+
+    }
+    cout << "smallest ="<< smallest <<endl;
+
+    return 0;
+}
