3bd0a3e0b1e3d5b399d1d9725c0247cbfb868108
somesh_codes <someshup007@gmail.com>
2026-09-26 23:59:27 +0530

    ed

--- array/majority.cpp
+++ array/majority.cpp
@@ -39,3 +39,14 @@ int main(){
 
     return 0;
 }
+
+    int nums[]= {5,10,15,20,25,30};
+    int size = 6;
+    int smallest = INT8_MAX;
+    for(int i=0;i<size;i++){
+        if(nums[i] < smallest){
+            smallest = nums[i];
+        }
+
+    }
+    cout << "smallest ="<< smallest <<endl;
